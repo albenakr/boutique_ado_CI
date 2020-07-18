22a92f83b2ef21703f2ac216961c52f98b1498c4
Albena Kr <albena.krasteva@gmail.com>
2020-07-18 19:32:10 +0000

    Added products views and templates

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,7 @@ from .models import Product, Category
 
 # Register your models here.
 
+
 # these classes extend the built in model admin class
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -22,11 +23,13 @@ class ProductAdmin(admin.ModelAdmin):
 # if you want to change the order of columns in the admin
 # change the order here in the list display attribute
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """A view to show all products, including sorting and searching"""
+ 
+    products = Product.objects.all()
+
+    # we need context since we need to send things back to the template
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
